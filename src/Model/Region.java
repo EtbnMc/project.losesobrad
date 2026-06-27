8862a9c01091b5b93b0b51bf94c2570a1210fa61
kianibenavides <kianibenavides@gmail.com>
2026-06-26 20:20:26 -0600

    Update Region.java

--- src/Model/Region.java
+++ src/Model/Region.java
@@ -1,6 +1,7 @@
 package Model;
-
-public class Region {
+import java.io.Serializable;
+public class Region implements Serializable {
+    private static final long serialVersionUID = 1L;
     private final String name;
     private final Map map;
     private final Enemy[] enemies;
